f09e21eeb222446aee585a2da490e13b0783ed42
Hans Jørgen Grimstad <hansj@lab5e.com>
2024-02-12 16:22:10 +0100

    First light

---
+++ Python/test.py
@@ -0,0 +1,156 @@
+import can
+import struct
+import math
+import time
+
+def id_fields(mode, a, b):
+    return (b & 0xff) | ((a & 0xffff) << 8) | ((mode & 0b11111) << 24)
+
+def float_to_u16(val):
+    mapped = int(val * (2**16))
+    if mapped < 0: mapped = 0
+    if mapped > 65535: mapped = 65535
+    return mapped
+
+def float_to_i16(val):
+    return float_to_u16((val + 1) / 2.0) - 32768
+
+class Motor:
+    REG_RUN_MODE = 0x7005
+    REG_IQ_REF = 0x7006
+    REG_SPD_REF = 0x700a
+    REG_LIMIT_TORQUE = 0x700b
+    REG_CUR_KP = 0x7010
+    REG_CUR_KI = 0x7011
+    REG_CUR_FILT_GAIN = 0x7014
+    REG_LOC_REF = 0x7016
+    REG_LIMIT_SPD = 0x7017
+    REG_LIMIT_CUR = 0x7018
+    REG_MECH_POS = 0x7019
+    REG_IQF = 0x701a
+    REG_MECHVEL = 0x701b
+    REG_VBUS = 0x701c
+    REG_ROTATION = 0x701d
+    REG_LOC_KP = 0x701e
+    REG_SPD_KP = 0x701f
+    REG_SPD_KI = 0x7020
+
+    RUN_MODE_OPERATIONAL = 0
+    RUN_MODE_POSITION = 1
+    RUN_MODE_SPEED = 2
+    RUN_MODE_CURRENT = 3
+
+    def __init__(self, bus, motor_id):
+        self.bus = bus
+        self.motor_id = motor_id
+
+    def enable(self):
+        msg = can.Message(
+            arbitration_id=id_fields(3, 0, self.motor_id),
+            data=[],
+            is_extended_id=True
+        )
+        try:
+            self.bus.send(msg)
+            print("Message sent on {}".format(self.bus.channel_info))
+        except can.CanError:
+            print("Message NOT sent")
+
+    def disable(self):
+        msg = can.Message(
+            arbitration_id=id_fields(4, 0, self.motor_id),
+            data=[],
+            is_extended_id=True
+        )
+        self.bus.send(msg)
+
+    def control(self, torque, target_angle, target_velocity, k_p, k_d):
+        """
+        torque: Torque to apply in Nm. From -12N to 12N.
+        target_angle: Angle to move to, from -4pi to 4pi.
+        target_velocity: Angular velocity to move to target. From -30rad/s to 30rad/s.
+        k_p: Proportional coeficient to Position PID controller. From 0.0 to 500.0.
+        k_d: Derivative coeficient to Position PID controller. From 0.0 to 5.0.
+        """
+
+        target_angle_num = float_to_u16(((target_angle / math.pi) + 4) / 8)
+        target_velocity_num = float_to_u16(((target_velocity / math.pi) + 30) / 60)
+        k_p_num = float_to_u16(k_p / 500)
+        k_d_num = float_to_u16(k_d / 5)
+
+        msg = can.Message(
+            arbitration_id=id_fields(1, float_to_i16(torque * 12), self.motor_id),
+            data=struct.pack(
+                "<HHHH",
+                target_angle_num,
+                target_velocity_num,
+                k_p_num,
+                k_d_num
+            ),
+            is_extended_id=True
+        )
+        self.bus.send(msg)
+
+    def param_write_typ(self, reg, typ, val):
+        msg = can.Message(
+            arbitration_id=id_fields(18, 0, self.motor_id),
+            data=struct.pack("<Hxx" + typ, reg, val),
+            is_extended_id=True
+        )
+        self.bus.send(msg)
+
+    def param_write_uint(self, reg, val):
+        return self.param_write_typ(reg, "I", val)
+
+    def param_write_int(self, reg, val):
+        return self.param_write_typ(reg, "i", val)
+
+    def param_write_float(self, reg, val):
+        return self.param_write_typ(reg, "f", val)
+
+#with can.Bus(interface='socketcan', channel='can0') as bus:
+#with can.interface.Bus(bustype='slcan', channel='COM1', baudrate='500000') as bus:
+with can.interface.Bus(bustype="slcan", channel="COM20") as bus:
+    print(bus)
+    print (bus.state)
+
+    m = Motor(bus, 0x7F)
+
+    try:
+        m.enable()
+
+        print("Enabled")
+
+        # Set to current control, set current ref
+        #m.param_write_uint(m.REG_RUN_MODE, m.RUN_MODE_CURRENT)
+        #m.param_write_float(m.REG_IQ_REF, 0.2)
+
+        # Set to position control, apply speed limit, go to location
+        m.param_write_uint(m.REG_RUN_MODE, m.RUN_MODE_POSITION)
+        m.param_write_float(m.REG_LIMIT_SPD, 30)
+        m.param_write_float(m.REG_LOC_REF, -3.1415 * 3 * 3.1 * 1)
+        #m.param_write_float(m.REG_LOC_REF, 2)
+
+        while True:
+            time.sleep(1)
+
+        # while True:
+        #    m.control(
+        #        torque=1,
+        #        target_angle=0,
+        #        target_velocity=0,
+        #        k_p=100,
+        #        k_d=1
+        #    )
+        #    time.sleep(1)
+
+        #    m.control(
+        #        torque=1,
+        #        target_angle=0,
+        #        target_velocity=0,
+        #        k_p=100,
+        #        k_d=1
+        #    )
+        #    time.sleep(1)
+    finally:
+        m.disable()
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# CyberGearPlayGround
+# "Hello CyberGear" in Python
+
+(Python example is courtesy of Hans Elias at HH)
+
+# Setup
+
+* pip install python-can (use socketcan or slcan interface).
+
+# Hardware required
+
+* Xiaomi CyberGear Motor.
+* [CANable-MKS](https://canable.io/getting-started.html) USB to CAN adapter. Adapter is also available from [AliExpress](https://www.aliexpress.com/w/wholesale-MKS-canable-pro.html?spm=a2g0o.productlist.search.0).
+* 24V Power Supply.
+* [Amass XT30(2+2)-F](https://www.china-amass.com/product/contain/1Yf5h7G4u1927079) cable. Cable with female connector is available from [AliExpress](https://www.aliexpress.com/w/wholesale-XT30(2%2B2)%2525252dF.html?spm=a2g0o.home.search.0)
+
+# Electrical connections
+
+![pinout](pictures/pinout.png)
+
+* Power supply: BAT+ / GND
+* MKS Canable: CAN_L / CAN_H ("G" on MKS Canable can be left unconnected)
+
+
+# Useful stuff
+
+* [cangaroo](https://github.com/normaldotcom/cangaroo/) open source can bus analyzer with support for transmit/receive of standard and FD frames and DBC decoding of incoming frames.
+* [canable](https://canable.io/getting-started.html) Getting started with MKS Canable.
+
+# Protocol implementations
+
+* [M5 Stack](https://github.com/project-sternbergia/cybergear_m5) (C++)
+* [CyberGearKit](https://github.com/CmST0us/CyberGearKit) (Swift)
